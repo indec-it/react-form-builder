fb284776d498c475bc74b43e1199722fcbd3cfb5
Matias Dumrauf <matias.dumrauf@gmail.com>
2019-05-17 19:30:50 -0300

    chore(husky): add 'chore' as a valid commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,6 +19,7 @@ module.exports = {
             [
                 'build',
                 'ci',
+                'chore',
                 'docs',
                 'feat',
                 'fix',
